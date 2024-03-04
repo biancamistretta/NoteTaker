83a50d8cb573ac3b0655d1fa96f4015bdfad3af2
biancamistretta <142269375+biancamistretta@users.noreply.github.com>
2024-03-04 18:04:03 -0500

    adding homework files

---
+++ Develop/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// API routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    newNote.id = generateId(); 
+    notes.push(newNote);
+    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Function to generate unique IDs
+function generateId() {
+  return Date.now().toString(); 
+}
